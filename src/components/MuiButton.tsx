7dc16c75a75e9866e7f1d8698c98bfdb6e537609
Zidane Innis <zidaneinnis@gmail.com>
2022-12-19 17:34:00 -0500

    Anchor tag and line spacing used

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Stack, Button } from "@mui/material";
+
+export const MuiButton = () => {
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text" href="https://google.com">
+        Text
+      </Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  );
+};
