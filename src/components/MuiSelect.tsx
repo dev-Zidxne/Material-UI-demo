b785ef953985ffa82ea55adc2e69e60f18267e73
Zidane Innis <zidaneinnis@gmail.com>
2022-12-21 18:55:53 -0500

    Created selct country component

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -1,5 +1,24 @@
-import React from "react";
-import { Box } from "@mui/system";
+import React, { useState } from "react";
+import { Box, TextField, MenuItem } from "@mui/material";
 export const MuiSelect = () => {
-  return <Box width="250px">MuiSelect</Box>;
+  const [country, setCountry] = useState("");
+  console.log({ country });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCountry(event.target.value as string);
+  };
+  return (
+    <Box width="250px">
+      <TextField
+        label="Select country"
+        select
+        value={country}
+        onChange={handleChange}
+        fullWidth
+      >
+        <MenuItem value="IN">India</MenuItem>
+        <MenuItem value="US">USA</MenuItem>
+        <MenuItem value="AU">Australia</MenuItem>
+      </TextField>
+    </Box>
+  );
 };
