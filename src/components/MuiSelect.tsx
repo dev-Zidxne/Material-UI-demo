20a64ce0382194b74b944eaf835776078c5c2a27
Zidane Innis <zidaneinnis@gmail.com>
2022-12-21 19:08:17 -0500

    React Material UI Tutorial - 8 - Select

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -1,21 +1,29 @@
 import React, { useState } from "react";
 import { Box, TextField, MenuItem } from "@mui/material";
 export const MuiSelect = () => {
-  const [country, setCountry] = useState("");
-  console.log({ country });
+  const [countries, setCountries] = useState<string[]>([]);
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCountry(event.target.value as string);
+    const value = event.target.value;
+    setCountries(typeof value === "string" ? value.split(",") : value);
   };
   return (
     <Box width="250px">
       <TextField
         label="Select country"
         select
-        value={country}
+        value={countries}
         onChange={handleChange}
         fullWidth
+        SelectProps={{
+          multiple: true,
+        }}
+        size="small"
+        color="secondary"
+        helperText="Please select your country"
+        error
       >
-        <MenuItem value="IN">India</MenuItem>
+        <MenuItem value="IN">Jamaica</MenuItem>
         <MenuItem value="US">USA</MenuItem>
         <MenuItem value="AU">Australia</MenuItem>
       </TextField>
