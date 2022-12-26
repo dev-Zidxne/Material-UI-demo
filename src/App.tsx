fa4e3039291631c28c43f4aba08e86791ea3b4e1
Zidane Innis <zidaneinnis@gmail.com>
2022-12-26 11:04:32 -0500

    Box Component uses

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { MuiCheckbox } from "./components/MuiCheckbox";
 import { MuiSwitch } from "./components/MuiSwitch";
 import { MuiRating } from "./components/MuiRating";
 import MuiAutoComplete from "./components/MuiAutoComplete";
+import { MuiLayout } from "./components/MuiLayout";
 
 function App() {
   return (
@@ -22,7 +23,8 @@ function App() {
       {/* <MuiCheckbox /> */}
       {/* <MuiSwitch /> */}
       {/* <MuiRating /> */}
-      <MuiAutoComplete />
+      {/* <MuiAutoComplete /> */}
+      <MuiLayout />
     </div>
   );
 }
