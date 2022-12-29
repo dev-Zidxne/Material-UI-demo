53389ad888080d09367cf4a3a2aa1c9dd9c56c98
Zidane Innis <zidaneinnis@gmail.com>
2022-12-29 12:54:47 -0500

    Icons, Deleted handler and Avatar explored w chips

--- src/components/MuiChip.tsx
+++ src/components/MuiChip.tsx
@@ -1,15 +1,34 @@
 import React from "react";
-import { Chip, Stack } from "@mui/material";
+import { Avatar, Chip, Stack } from "@mui/material";
+import FaceIcon from "@mui/icons-material/Face";
 
 const MuiChip = () => {
   return (
     <Stack direction="row" spacing={1}>
-      <Chip label="Chip" color="primary" size="small" />
+      <Chip label="Chip" color="primary" size="small" icon={<FaceIcon />} />
       <Chip
         label="Chip Outlined"
         color="primary"
         size="small"
         variant="outlined"
+        avatar={<Avatar />}
+      />
+      <Chip
+        label="Click"
+        color="success"
+        onClick={() => {
+          alert("Clicked!");
+        }}
+      />
+      <Chip
+        label="Delete"
+        color="error"
+        onClick={() => {
+          alert("Clicked!");
+        }}
+        onDelete={() => {
+          alert("Delete Handler Called");
+        }}
       />
     </Stack>
   );
