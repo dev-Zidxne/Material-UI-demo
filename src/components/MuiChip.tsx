c42cc721025f250b147266371b51cdedd75ab517
Zidane Innis <zidaneinnis@gmail.com>
2022-12-29 12:49:20 -0500

    Size and variant props

--- src/components/MuiChip.tsx
+++ src/components/MuiChip.tsx
@@ -4,7 +4,13 @@ import { Chip, Stack } from "@mui/material";
 const MuiChip = () => {
   return (
     <Stack direction="row" spacing={1}>
-      <Chip label="Chip" color="primary" />
+      <Chip label="Chip" color="primary" size="small" />
+      <Chip
+        label="Chip Outlined"
+        color="primary"
+        size="small"
+        variant="outlined"
+      />
     </Stack>
   );
 };
