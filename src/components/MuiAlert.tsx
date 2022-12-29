806953fa5df4d88cde5a382b2d9da2340cdba952
Zidane Innis <zidaneinnis@gmail.com>
2022-12-29 15:40:59 -0500

    Alert title

--- src/components/MuiAlert.tsx
+++ src/components/MuiAlert.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Stack, Alert } from "@mui/material";
+import { Stack, Alert, AlertTitle } from "@mui/material";
 
 const MuiAlert = () => {
   return (
@@ -8,6 +8,36 @@ const MuiAlert = () => {
       <Alert severity="warning">This is a warning alert</Alert>
       <Alert severity="info">This is a info alert</Alert>
       <Alert severity="success">This a success alert</Alert>
+
+      <Alert variant="outlined" severity="error">
+        This is an error alert
+      </Alert>
+      <Alert variant="outlined" severity="warning">
+        This is a warning alert
+      </Alert>
+      <Alert variant="outlined" severity="info">
+        This is a info alert
+      </Alert>
+      <Alert variant="outlined" severity="success">
+        This a success alert
+      </Alert>
+
+      <Alert variant="filled" severity="error">
+        <AlertTitle>Error</AlertTitle>
+        This is an error alert
+      </Alert>
+      <Alert variant="filled" severity="warning">
+        <AlertTitle>Warning</AlertTitle>
+        This is a warning alert
+      </Alert>
+      <Alert variant="filled" severity="info">
+        <AlertTitle>Info</AlertTitle>
+        This is a info alert
+      </Alert>
+      <Alert variant="filled" severity="success">
+        <AlertTitle>Success</AlertTitle>
+        This a success alert
+      </Alert>
     </Stack>
   );
 };
